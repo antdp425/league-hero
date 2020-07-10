d22aeabf6a2065c217e56cac6cd932d31e2d04f7
antdp425 <antdp425@aol.com>
2020-07-10 15:13:52 -0400

    seeds db with league and team data

--- league-hero-backend/db/seeds.rb
+++ league-hero-backend/db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+   League.first.teams.create(name: Faker::Team.name.titleize,
+       email: Faker::Internet.email,
+        phone: Faker::PhoneNumber.cell_phone)
+   League.second.teams.create(name: Faker::Team.name.titleize,
+       email: Faker::Internet.email,
+        phone: Faker::PhoneNumber.cell_phone)
+   League.third.teams.create(name: Faker::Team.name.titleize,
+       email: Faker::Internet.email,
+        phone: Faker::PhoneNumber.cell_phone)
+   League.fourth.teams.create(name: Faker::Team.name.titleize,
+       email: Faker::Internet.email,
+        phone: Faker::PhoneNumber.cell_phone)
+   League.fifth.teams.create(name: Faker::Team.name.titleize,
+       email: Faker::Internet.email,
+        phone: Faker::PhoneNumber.cell_phone)
+end
