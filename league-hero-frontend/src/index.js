e1ec78e40401069bada2a86f8aa627f296f69ed4
antdp425 <antdp425@aol.com>
2020-07-10 19:16:30 -0400

    adds static methods for newLeagueForm and newLeagueListener

--- league-hero-frontend/src/index.js
+++ league-hero-frontend/src/index.js
@@ -17,19 +17,21 @@ class League{
       .then (resp =>  resp.json())
       .then (leagues => {
          let container = document.querySelector(".container-fluid")
-         let leagueRow = document.createElement("div")
          let actionRow = document.createElement("div")
-         
-         leagueRow.id = "league-rows"
-         leagueRow.className = "row"
+         let leagueRow = document.createElement("div")
 
          actionRow.id = "action-row"
          actionRow.className = "row"
+         
+         leagueRow.id = "league-rows"
+         leagueRow.className = "row"
          
          container.appendChild(actionRow)
          container.appendChild(leagueRow)
 
          actionRow.innerHTML += League.newLeagueButton()
+         League.newLeagueListener()
+
          leagues.forEach(league => {
             let l = new League(league)
             leagueRow.innerHTML += l.renderLeague()
@@ -53,8 +55,36 @@ class League{
 
    static newLeagueButton(){
       return `
-      <button type="button" class="btn btn-block btn-outline-primary    ml-auto">Add League</button>
+      <button type="button" class="btn btn-block btn-outline-primary ml-auto" id="add-league">Add League</button>
       `
+   }
+
+   static newLeagueForm(){
+      return `      
+         <form>
+            <div class="form-group">
+               <label for="exampleInputEmail1">Email address</label>
+               <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+               <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            </div>
+            <div class="form-group">
+               <label for="exampleInputPassword1">Password</label>
+               <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+            </div>
+            <div class="form-check">
+               <input type="checkbox" class="form-check-input" id="exampleCheck1">
+               <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+         </form>
+      `
+   }
+
+   static newLeagueListener(){
+      let button = document.querySelector("#add-league")
+      button.addEventListener("click", () => {
+         event.target.parentElement.innerHTML += League.newLeagueForm()
+      })
    }
 
 }
