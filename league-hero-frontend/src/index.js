271d7272ea59ffc59c25dceef69c2c9a7f5654a6
antdp425 <antdp425@aol.com>
2020-07-10 13:11:18 -0400

    updates renderLeague to instance method and creates renderLeagueInfo instance method

--- league-hero-frontend/src/index.js
+++ league-hero-frontend/src/index.js
@@ -5,23 +5,39 @@ document.addEventListener("DOMContentLoaded",()=>{
 })
 
 class League{
-   constructor(name, format, start, end){
-      this.name = name
-      this.format = format
-      this.start = start
-      this.end = end
+   constructor(leagueInfo){
+      this.name = leagueInfo.name
+      this.format = leagueInfo.format
+      this.start = leagueInfo.start
+      this.end = leagueInfo.end
    }
 
    static getLeagues(){
       return fetch(`${baseURL}/leagues`)
       .then (resp =>  resp.json())
-      .then (leagues => League.renderLeagues(leagues))
+      .then (leagues => {
+         leagues.forEach(league => {
+            let l = new League(league)
+            l.renderLeague()
+         })
+      })
    }
 
-   static renderLeagues(leagues){
-      leagues.forEach(el => {
-         console.log(el)
-         console.log(el)
-      });
+   renderLeague(){
+      let container = document.querySelector(".container-fluid")
+      let card = this.renderLeagueInfo()
+      container.innerHTML += card
+   }
+
+   renderLeagueInfo(){
+      return `
+         <div class="card bg-light mb-3" style="max-width: 18rem;">
+            <div class="card-header">${this.name}</div>
+               <div class="card-body">
+                  <h5 class="card-title">Light card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+               </div>
+         </div>
+      `
    }
 }
