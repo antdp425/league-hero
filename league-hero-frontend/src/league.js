716dfb09b97a443e4394c5de6f90dca8959c231e
antdp425 <antdp425@aol.com>
2020-07-14 12:39:10 -0400

    creates skeleton layout for leagues card w/ date

--- league-hero-frontend/src/league.js
+++ league-hero-frontend/src/league.js
@@ -4,8 +4,8 @@ class League{
       this.id = leagueInfo.id
       this.name = leagueInfo.name
       this.league_format = leagueInfo.league_format
-      this.start_date = leagueInfo.start_date
-      this.end_date = leagueInfo.end_date
+      this.start_date = new Date(leagueInfo.start_date).getUTCDate()
+      this.end_date = new Date(leagueInfo.end_date)
       this.teams = leagueInfo.teams
    }
 
@@ -39,17 +39,7 @@ class League{
 
       })
    }
-
-   static addListeners(){
-      let leagues = document.querySelector("#league-rows")
-      leagues.addEventListener("click",()=>{      
-         if (!!(event.target.dataset.leagueId)){   
-         let leagueId = event.target.dataset.leagueId
-         this.getLeague(leagueId)
-         }
-      })
-   }
-
+   
    static getLeague(leagueId){
       fetch(`${baseURL}/leagues/${leagueId}`)
       .then (resp =>  resp.json())
@@ -75,6 +65,15 @@ class League{
 
       })
    }
+   static addListeners(){
+      let leagues = document.querySelector("#league-rows")
+      leagues.addEventListener("click",()=>{      
+         if (!!(event.target.dataset.leagueId)){   
+         let leagueId = event.target.dataset.leagueId
+         this.getLeague(leagueId)
+         }
+      })
+   }
 
    static addActionListeners(){
       let actions = document.querySelector(".action-buttons")
@@ -83,9 +82,11 @@ class League{
          switch (true) {
             case event.target.dataset.action === "edit":
                console.log("Its an edit")
-               // let leagueId = event.target.nextElementSibling.firstElementChild.dataset.leagueId
-               // let teamId = event.target.dataset.teamId
-               // this.getTeam(leagueId, teamId)
+               if (document.querySelector("form")){                  
+               } else {
+                  event.target.parentElement.parentElement.parentElement.parentElement.parentElement.insertAdjacentHTML("beforeend",this.editLegueForm())
+                  this.updateLeagueListener()
+               }
                break;
             case event.target.dataset.action === "delete":
                this.deleteLeague(leagueId)
@@ -93,6 +94,70 @@ class League{
                break;
          }
       })
+   }
+
+   static editLeagueForm(){
+      // let teamData = {
+      //    id: document.querySelector("[data-team-id]").dataset.teamId,
+      //    name: document.querySelector("#team_name").innerText ,
+      //    email: document.querySelector("#team_email").innerText ,
+      //    phone: document.querySelector("#team_phone").innerText
+      // }
+      // return `      
+      //    <form>
+      //       <div class="form-group">
+      //          <label for="team_name">Team Name:</label>
+      //          <input type="text" class="form-control" name="team_name" id="team_name" value="${teamData.name}">
+      //       </div>
+      //       <div class="form-group">
+      //          <label for="email">Email:</label>
+      //          <input type="email" class="form-control" name="email" id="team_email" value="${teamData.email}">
+      //       </div>
+      //       <div class="form-group">
+      //       <label for="phone">Phone:</label>
+      //       <input type="text" class="form-control" name="phone" id="team_phone" maxlength="10" value="${teamData.phone}">
+      //    </div>
+      //       <button type="submit" class="btn btn-block btn-primary" data-team-id="${teamData.id}" id="update">Update</button>
+      //    </form>
+      // `
+   }
+
+   static updateLeagueListener(){
+   //    let button = document.querySelector("#update")
+   //    button.addEventListener("click", () => {
+   //       event.preventDefault()
+   //       this.updateTeam(event.target.dataset.teamId)
+   //       button.parentNode.removeChild(button)
+   // })
+}
+
+   static updateLeague(leagueId){
+      // console.log(`You passed in ${teamId}`);
+      // let form = event.target.parentElement
+      // let formData = {
+      //    name: form[0].value,
+      //    email: form[1].value,
+      //    phone: form[2].value
+      // }
+
+      // let configObj = {
+      //    method: "PATCH",
+      //    headers: {
+      //       "Content-Type": "application/json",
+      //       "Accept": "application/json"
+      //    },
+      //    body: JSON.stringify(formData)
+      // }
+      
+      // fetch(`${baseURL}/teams/${teamId}`, configObj)
+      // .then(resp => resp.json())
+      // .then(created => {
+      //    let teamRow = document.querySelector("#team-rows")
+         
+      //    let t = new Team(created)
+      //    teamRow.innerHTML = ""
+      //    teamRow.innerHTML += t.renderTeam()
+      // })
    }
 
    static deleteLeague(leagueId){
@@ -117,17 +182,25 @@ class League{
       League.store.call(this)
 
       return `
-      <div class="col-xs-10 col-sm-6 col-md-4">
-         <div class="card bg-light mb-3">
-            <div class="card-header">
-            <a href="#" data-league-id="${this.id}">${this.name}</a>
-            </div>
-               <div class="card-body">
-                  <h5 class="card-title">Light card title</h5>
-                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-               </div>
+      <div class="col-xs-12 col-sm-11 col-md-6 col-lg-6">
+      <div class="card bg-light mb-3">
+         <div class="card-header bg-border-dark">
+         <a href="#" data-league-id="${this.id}">${this.name}</a>
          </div>
+            <div class="card-body bg-light border-dark">
+              <div class="row">
+                <div class="col">
+                  <h6 class="card-title bg-secondary text-center date-month">Month Here</h6>
+                <h4 class="card-text bg-dark text-center date-number">${this.start_date}</h4>
+               </div>
+                <div class="col">
+                  <h5 class="card-title">Light card title</h5>
+               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+              </div>
+            </div>
       </div>
+   </div>
       `
    }
 
